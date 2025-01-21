4ca24fd682cafc070346edf13feb848213556dbb
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-01-21 07:51:38 +0000

    APIS-7445 - Add the auth code

---
+++ test/uk/gov/hmrc/apiplatform/modules/gkauth/services/StrideAuthorisationServiceMockModule.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.gkauth.services
+
+import scala.concurrent.Future
+import scala.concurrent.Future.{failed, successful}
+
+import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
+
+import play.api.mvc.Results._
+import play.api.mvc.{MessagesRequest, Result}
+import uk.gov.hmrc.auth.core.InvalidBearerToken
+
+import uk.gov.hmrc.apiplatform.modules.gkauth.domain.models.{GatekeeperStrideRole, LoggedInRequest}
+
+trait StrideAuthorisationServiceMockModule {
+  self: MockitoSugar with ArgumentMatchersSugar =>
+
+  protected trait BaseStrideAuthorisationServiceMock {
+    def aMock: StrideAuthorisationService
+
+    object Auth {
+
+      private def wrap[A](fn: MessagesRequest[A] => Future[Either[Result, LoggedInRequest[A]]]) = {
+        when(aMock.refineStride[A](*)).thenReturn(fn)
+      }
+
+      def succeeds[A](role: GatekeeperStrideRole) = {
+        wrap[A]((msg) => successful(Right(new LoggedInRequest(Some("Bobby Example"), role, msg))))
+      }
+
+      def invalidBearerToken[A]() = {
+        wrap[A]((msg) => failed(new InvalidBearerToken))
+      }
+
+      def hasInsufficientEnrolments[A]() = {
+        wrap[A]((msg) => successful(Left(Forbidden("You do not have permission"))))
+      }
+
+      def sessionRecordNotFound[A]() = {
+        wrap[A]((msg) => successful(Left(Redirect("http://example.com"))))
+      }
+    }
+  }
+
+  object StrideAuthorisationServiceMock extends BaseStrideAuthorisationServiceMock {
+    val aMock = mock[StrideAuthorisationService]
+  }
+
+}
---
+++ test/uk/gov/hmrc/apiplatform/modules/gkauth/services/StrideAuthorisationServiceSpec.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatform.modules.gkauth.services
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+import play.api.http.HeaderNames.LOCATION
+import play.api.http.Status._
+import play.api.mvc.Results._
+import play.api.mvc.{MessagesRequest, Result}
+import play.api.test.{DefaultAwaitTimeout, FakeRequest, FutureAwaits, StubMessagesFactory}
+
+import uk.gov.hmrc.apiplatform.modules.common.utils.HmrcSpec
+import uk.gov.hmrc.apiplatform.modules.gkauth.config.{StrideAuthConfig, StrideAuthRoles}
+import uk.gov.hmrc.apiplatform.modules.gkauth.connectors.StrideAuthConnectorMockModule
+import uk.gov.hmrc.apiplatform.modules.gkauth.controllers.actions.ForbiddenHandler
+import uk.gov.hmrc.apiplatform.modules.gkauth.domain.models.{GatekeeperRole, GatekeeperRoles, LoggedInRequest}
+
+class StrideAuthorisationServiceSpec extends HmrcSpec with DefaultAwaitTimeout with FutureAwaits with StrideAuthConnectorMockModule with StubMessagesFactory
+    with TableDrivenPropertyChecks {
+  val strideAuthRoles = StrideAuthRoles(adminRole = "test-admin", superUserRole = "test-superUser", userRole = "test-user")
+  val fakeRequest     = FakeRequest()
+  val msgRequest      = new MessagesRequest(fakeRequest, stubMessagesApi())
+
+  trait Setup {
+    val strideAuthConfig = StrideAuthConfig(strideLoginUrl = "http:///www.example.com", successUrlBase = "", origin = "", roles = strideAuthRoles)
+
+    val underTest = new StrideAuthorisationService(
+      strideAuthConnector = StrideAuthConnectorMock.aMock,
+      forbiddenHandler = new ForbiddenHandler {
+        def handle(msgResult: MessagesRequest[_]): Result = Forbidden("No thanks")
+      },
+      strideAuthConfig = strideAuthConfig
+    )
+  }
+
+  "createStrideRefiner" should {
+    "return the appropriate results" in new Setup {
+
+      import GatekeeperRoles._
+
+      val cases = Table(
+        ("requiredRole", "user has role", "expected outcome"),
+        (ADMIN, ADMIN, Right(ADMIN)),
+        (SUPERUSER, ADMIN, Right(ADMIN)),
+        (USER, ADMIN, Right(ADMIN)),
+        (ADMIN, SUPERUSER, Left(FORBIDDEN)),
+        (SUPERUSER, SUPERUSER, Right(SUPERUSER)),
+        (USER, SUPERUSER, Right(SUPERUSER)),
+        (ADMIN, USER, Left(FORBIDDEN)),
+        (SUPERUSER, USER, Left(FORBIDDEN)),
+        (USER, USER, Right(USER))
+      )
+
+      forAll(cases) { case (requiredRole, userIsOfRole, expected) =>
+        StrideAuthConnectorMock.Authorise.returnsFor(userIsOfRole)
+
+        val result: Either[Result, LoggedInRequest[_]] = await(underTest.refineStride(requiredRole)(msgRequest))
+        (result, expected) match {
+          case (Right(request: LoggedInRequest[_]), Right(expectedRole: GatekeeperRole)) => request.role shouldBe expectedRole
+          case (Left(result: Result), Left(expectedCode: Int))                           => result.header.status shouldBe expectedCode
+          case _                                                                         => fail()
+        }
+      }
+    }
+
+    "return a redirect when there is no active session" in new Setup {
+      StrideAuthConnectorMock.Authorise.failsWithNoActiveSession
+
+      val result: Either[Result, LoggedInRequest[_]] = await(underTest.refineStride(GatekeeperRoles.USER)(msgRequest))
+
+      result.left.value.header.status shouldBe SEE_OTHER
+      result.left.value.header.headers(LOCATION) should startWith(strideAuthConfig.strideLoginUrl)
+      result.left.value.header.headers(LOCATION) should include(s"successURL=${strideAuthConfig.successUrlBase}")
+    }
+  }
+}
