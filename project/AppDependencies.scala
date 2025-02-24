f364ccecbb3f6fa940769fad2730b1c3ddfe18d5
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-02-24 15:46:16 +0000

    APIS-7489 - Migrate to using the domain library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,22 @@ object AppDependencies {
 
   private val bootstrapVersion = "9.7.0"
   val commonDomainVersion      = "0.18.0"
-
+  private val orgDomainVersion = "0.1.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc" %% "api-platform-common-domain"   % commonDomainVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"   % "11.11.0",
-    "uk.gov.hmrc" %% "internal-auth-client-play-30" % "3.0.0",
-    "commons-validator"  % "commons-validator"          % "1.7"
-
+    "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
+    "uk.gov.hmrc"      %% "api-platform-common-domain"       % commonDomainVersion,
+    "uk.gov.hmrc"      %% "api-platform-organisation-domain" % orgDomainVersion,
+    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "11.11.0",
+    "uk.gov.hmrc"      %% "internal-auth-client-play-30"     % "3.0.0",
+    "commons-validator" % "commons-validator"                % "1.7"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30"              % bootstrapVersion,
-    "org.jsoup"    % "jsoup"                               % "1.18.3",
-    "uk.gov.hmrc" %% "api-platform-common-domain-fixtures" % commonDomainVersion
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"                    % bootstrapVersion,
+    "org.jsoup"    % "jsoup"                                     % "1.18.3",
+    "uk.gov.hmrc" %% "api-platform-common-domain-fixtures"       % commonDomainVersion,
+    "uk.gov.hmrc" %% "api-platform-organisation-domain-fixtures" % orgDomainVersion
   ).map(_ % "test")
 
   val it = Seq.empty
