705a62abdc2961157831100d12801e14ca21fe0a
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-03-07 10:27:19 +0000

    APIS-7467 - Bump domain version (#7)
    * APIS-7467 - Bump domain version
    
    * APIS-7467 - PR Bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val orgDomainVersion = "0.1.0"
+  private val bootstrapVersion = "9.10.0"
+  private val orgDomainVersion = "0.5.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
