2625a4a5a417bebcfb89d54f1ae4920d93ed790a
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-04-10 16:39:35 +0100

    APIS-7536 Add new field organisationType to Organisation (#10)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.11.0"
-  private val orgDomainVersion = "0.6.0"
+  private val orgDomainVersion = "0.7.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
