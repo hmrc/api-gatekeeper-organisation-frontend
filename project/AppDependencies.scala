1ed83f2e758abb00b238473cc14defc8fd50d918
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-12-15 09:18:28 +0000

    pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.4.0"
   private val orgDomainVersion = "0.11.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-frontend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc"      %% "api-platform-organisation-domain" % orgDomainVersion,
-    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "12.7.0",
+    "uk.gov.hmrc"      %% "play-frontend-hmrc-play-30"       % "12.22.0",
     "uk.gov.hmrc"      %% "internal-auth-client-play-30"     % "3.1.0",
     "commons-validator" % "commons-validator"                % "1.7",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"               % hmrcMongoVersion
