32d63f58858d2e319ed0bd904cdce18a8f835d55
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-07 11:28:57 +0100

    APIS-8291 Add new config item to disable adding to the organisation registration allow list

--- app/uk/gov/hmrc/apigatekeeperorganisationfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/apigatekeeperorganisationfrontend/config/AppConfig.scala
@@ -23,5 +23,5 @@ import play.api.Configuration
 @Singleton
 class AppConfig @Inject() (config: Configuration) {
   val welshLanguageSupportEnabled: Boolean = config.getOptional[Boolean]("features.welsh-language-support").getOrElse(false)
-
+  val disableAddToAllowList: Boolean       = config.getOptional[Boolean]("disable-add-to-allow-list").getOrElse(false)
 }
